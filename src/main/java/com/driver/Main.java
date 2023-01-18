dbd3491170f10192cd20b4391bea81ef14e4e806
Swati <swatijangra1994@gmail.com>
2023-01-18 16:33:44 +0530

    third

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,21 +1,25 @@
 package com.driver;
 
 public class Main {
+    public static class A {
+        public String meth() {
+
+            return "Invoking method from class A";
+        }
+    }
+    static class B extends A {
+        @Override
+        public String meth() {
+
+            return "Method is overridden in Extendend class B";
+        }
+    }
+
     public static void main(String[] args) {
-        B b1=new A();
+        B b1=new B();
         System.out.println(b1.meth());
         B b=new B();
         System.out.println(b.meth());
     }
 }
-class A {
-    public String meth() {
-        return "Invoking method from class A";
-    }
 
-}
-class B extends A {
-    public String meth() {
-        return "Method is overridden in Extendend class B";
-    }
-}
