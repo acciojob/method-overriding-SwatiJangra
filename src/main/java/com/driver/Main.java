be1ff6bb31ec832602c58489719c939606ba9b40
swati <swatijangra199@gmail.com>
2023-02-07 00:04:51 +0530

    commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -12,13 +12,12 @@ public class Main {
             return "Method is overridden in Extendend class B";
         }
     }
-
     public static void main(String[] args) {
-        B b1=new B();
-        b1.meth();
-
         B b=new B();
         b.meth();
+
+        B b2=new B();
+        b2.meth();
     }
 }
 
